5991f39cd6ac8c1d9be228480d0d355252fad4a1
Martin Wisniowski <mw@node3000.com>
2017-06-14 16:29:28 +0200

    Add pico-8 palette as data-source

--- src/components/Palette.vue
+++ src/components/Palette.vue
@@ -1,13 +1,30 @@
 <template>
-  <canvas v-bind:style='{ backgroundColor: color }' />
+  <div>
+    <canvas v-bind:style='{ backgroundColor: color }' @click='pickColor' />
+    <canvas v-bind:style='{ backgroundColor: color }' />
+    <canvas v-bind:style='{ backgroundColor: color }' />
+    <canvas v-bind:style='{ backgroundColor: color }' />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'palette',
+  methods: {
+    pickColor (event) {
+      console.log('pickColor called')
+      console.log(event.target.style.backgroundColor)
+    }
+  },
   data () {
     return {
-      color: '#ff7788'
+      color: '#ff7788',
+      colors: {
+        pico8:
+        [ '#000000', '#1D2B53', '#7E2553', '#008751',
+          '#AB5236', '#5F574F', '#C2C3C7', '#FFF1E8',
+          '#FF004D', '#FFA300', '#FFEC27', '#00E436',
+          '#29ADFF', '#83769C', '#FF77A8', '#FFCCAA' ]}
     }
   }
 }
