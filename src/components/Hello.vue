67cf6a8a72403e3a75f7b81979bd0f24ae3be65e
Martin Wisniowski <mw@node3000.com>
2017-06-14 13:58:38 +0200

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+  hallo
+    <Pixel />
+  </div>
+</template>
+
+<script>
+
+import Pixel from '@/components/Pixel'
+
+export default {
+  name: 'hello',
+  components: {
+    Pixel
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Pixel.vue
@@ -0,0 +1,18 @@
+<template>
+    <canvas :style='style'></canvas>
+</template>
+
+<script>
+export default {
+  name: 'pixel',
+  data: function () {
+    return {
+      color: '#ff0000',
+      style: 'backgroundColor: ' + this.color
+    }
+  }
+}
+</script>
+
+<style>
+</style>
